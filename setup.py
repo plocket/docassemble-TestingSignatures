70ecdaf552da1824dc5c26a2109c63dfc9d7045c
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-07-29 09:57:27 -0400

    Restore to idempotency issue (though through different code)

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.TestingSignatures',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.TestingSignatures\n\nA docassemble extension.\n\n## Author\n\nmb.restless.tech@gmail.com\n\n',
+      long_description='# docassemble.TestingSignatures\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nmb.restless.tech@gmail.com\r\n\r\n',
       long_description_content_type='text/markdown',
       author='',
       author_email='mb.restless.tech@gmail.com',
